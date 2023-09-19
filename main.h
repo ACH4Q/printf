95724421e526d60ee4990e8f26c79da1f23fcb6c
ACH4Q <achaqmohammedamine1@gmail.com>
2023-09-19 09:28:05 -0700

    main.h

--- main.h
+++ main.h
@@ -151,44 +151,4 @@ int flags, int width, int precision, int size);
 
 int get_flags(const char *format, int *i);
 
-int get_width(const char *format, int *i, va_list list);
-
-int get_precision(const char *format, int *i, va_list list);
-
-int get_size(const char *format, int *i);
-
-
-
-/*Function to print string in reverse*/
-
-int print_reverse(va_list types, char buffer[],
-
-int flags, int width, int precision, int size);
-
-
-
-/*Function to print a string in rot 13*/
-
-int print_rot13string(va_list types, char buffer[],
-
-int flags, int width, int precision, int size);
-
-
-
-/* width handler */
-
-int handle_write_char(char c, char buffer[],
-
-int flags, int width, int precision, int size);
-
-int write_number(int is_positive, int ind, char buffer[],
-
-int flags, int width, int precision, int size);
-
-int write_num(int ind, char bff[], int flags, int width, int precision,
-
-int length, char padd, char extra_c);
-
-int write_pointer(char buffer[], int ind, int length,
-
-int width, int flags, char padd, char extra_c, i
+int get_width(const char *format,
