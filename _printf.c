e4bdcd8849377c0477f076bce922cd8ec56c66ae
lailahajji <hajjileila58@gmail.com>
2023-09-19 06:02:33 -0700

    fixing arguments null

--- _printf.c
+++ _printf.c
@@ -1,37 +1,34 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * _printf - prints according to a given format. printf Implementation.
+ * @format: string holding characters and format of arguments to be printed.
+ *
+ * Return: number of charaters printed.
  */
+
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-	conver_t f_list[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
-	};
-	va_list arg_list;
+	va_list original, copy;
+	int n_printed = 0, error = 0;
 
-	if (format == NULL)
+	if (format == NULL || *format == '\0')
 		return (-1);
 
-	va_start(arg_list, format);
-	/*Calling parser function*/
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+	va_start(original, format);
+	va_copy(copy, original);
+
+	error = check_input(format, &original);
+
+	if (error != 0)
+	{
+		return (error * -1);
+	}
+
+	n_printed = print_output(format, &copy);
+
+	va_end(original);
+	va_end(copy);
+
+	return (n_printed);
 }
